4ec9e9a379cd85bf5da443a9b483965b712e2689
Szymon Gałaś <szymon.galas@microsoft.wsei.edu.pl>
2022-02-02 12:15:47 +0100

    <Required>

---
+++ 13210-egzamin-z-asp.net/Controllers/IndeksController.cs
@@ -0,0 +1,34 @@
+﻿using _13210_egzamin_z_asp.net.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13210_egzamin_z_asp.net.Controllers
+{
+    public class IndeksController : Controller
+    {
+        static List<StudentsGrades> studentsGrades = new List<StudentsGrades>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(StudentsGrades _studentsGrades)
+        {
+            if (ModelState.IsValid)
+            {
+                studentsGrades.Add(_studentsGrades);
+                return View("List", studentsGrades);
+            }
+            else
+            {
+                return View(_studentsGrades);
+            }
+        }
+
+
+    }
+}
---
+++ 13210-egzamin-z-asp.net/Models/StudentsGrades.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _13210_egzamin_z_asp.net.Models
+{
+    public class StudentsGrades
+    {
+        [Required]
+        public int StudentsIndex { get; set; }
+        [Required]
+        [Range(2, 5, ErrorMessage = "Grade must be between 2 and 5")]
+        public decimal Grade { get; set; }
+        [Required]
+        public string Subject { get; set; }
+    }
+}
